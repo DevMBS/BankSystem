c78b9e720c60b62f120c1edda191a50818e21157
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-05-08 00:46:55 +0300

    Delete cowsay.js

--- cowsay.js
+++
@@ -1,8 +0,0 @@
-//это я сделал просто по-приколу, типа чтобы корова в терминале говорила, что сервер стартует)
-var cowsay = require("cowsay");
-
-console.log(cowsay.say({
-	text : "Server launches...",
-	e : "oO",
-	T : "U "
-}));
